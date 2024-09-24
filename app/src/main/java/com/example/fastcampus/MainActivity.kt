b23037f6cff769536fc1aee6f8c0f6bc0f5305af
kish0ck <kshong@hnsmall.com>
2024-09-24 20:19:58 +0900

    2024/09/24

--- app/src/main/java/com/example/fastcampus/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fastcampus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.drawable)
+        setContentView(R.layout.activity_main)
+        // 한번만 하면 되는 작업
+        Log.d("LifeCycle","onCreate")
 
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle","onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle","onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle","onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle","onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle","onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle","onRestart")
+        super.onRestart()
     }
 }
